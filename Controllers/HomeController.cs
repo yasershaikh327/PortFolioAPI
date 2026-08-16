b10451426013e1565cd45c6269fec906c4320422
Yaser Shaikh <syaser327@gmail.com>
2026-08-16 16:32:22 +0530

    sad

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace PortFolioAPI.Controllers
             var message = new StringContent("Good Morning 🌞", Encoding.UTF8, "text/plain");
 
             // "mytopic" is any name you choose
-            var response = await client.PostAsync("https://ntfy.sh/yasershaikh327topic", message);
+            var response = await client.PostAsync("https://ntfy.sh/MyPortfolioVisitors327", message);
 
             return Json(new { status = response.StatusCode });
         }
