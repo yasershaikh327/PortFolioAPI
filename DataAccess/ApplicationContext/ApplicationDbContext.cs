89e5a15413d348aaa8fe75186bb8a0ee961adfb2
Yaser Shaikh <syaser327@gmail.com>
2026-08-16 19:35:10 +0530

    Added cdataacess

---
+++ DataAccess/ApplicationContext/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using DataAccess.Dto;
+using Microsoft.EntityFrameworkCore;
+using PortFolioAPI.Models;
+
+namespace PortFolioAPI.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Viewer> viewers_list { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Viewer>(entity =>
+            {
+                entity.HasKey(e => e.id);
+                entity.Property(e => e.id).ValueGeneratedOnAdd();
+            });
+        }
+    }
+}
---
+++ DataAccess/Dto/Viewer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Dto
+{
+    public class Viewer
+    {
+        public int id { get; set; }
+        public string country_code { get; set; }
+        public string country_name { get; set; }
+        public string city { get; set; }
+        public string timezone { get; set; }
+        public string device_type { get; set; }
+        public string operating_system { get; set; }
+        public string browser { get; set; }
+        public string user_agent { get; set; }
+        public string page_url { get; set; }
+        public string referrer { get; set; }
+        public DateTime visit_time { get; set; }
+    }
+}
