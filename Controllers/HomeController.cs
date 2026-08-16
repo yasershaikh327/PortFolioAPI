d21b616566e75c6540896e6d6906a970c10c71fa
Yaser Shaikh <syaser327@gmail.com>
2026-08-16 16:48:47 +0530

    asd

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -19,12 +19,20 @@ namespace PortFolioAPI.Controllers
         public async Task<IActionResult> Notify()
         {
             using var client = new HttpClient();
-            var message = new StringContent("Good Morning 🌞", Encoding.UTF8, "text/plain");
 
-            // Send notification to ntfy public server, topic: yasershaikhportfoliovisitors327
+            // Explicitly set Content-Type to text/plain
+            var message = new StringContent("Good Morning 🌞", Encoding.UTF8, "text/plain");
+            message.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("text/plain");
+
             var response = await client.PostAsync("https://ntfy.sh/yasershaikhportfoliovisitors327", message);
 
-            return Json(new { status = response.StatusCode });
+            var responseText = await response.Content.ReadAsStringAsync();
+
+            return Json(new
+            {
+                status = response.StatusCode,
+                response = responseText
+            });
         }
     }
 }
