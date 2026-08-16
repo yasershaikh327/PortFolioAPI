8576eb7c755504d1b642b1e7844bb958ab29f113
Yaser Shaikh <syaser327@gmail.com>
2026-08-16 16:17:25 +0530

    Test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PortFolioAPI.Models;
 using System.Diagnostics;
+using System.Text;
 
 namespace PortFolioAPI.Controllers
 {
@@ -15,6 +16,19 @@ namespace PortFolioAPI.Controllers
         {
             return Json(new { status = "ok" });
         }
+
+        [Route("notify")]
+        public async Task<IActionResult> Notify()
+        {
+            using var client = new HttpClient();
+            var message = new StringContent("Good Morning 🌞", Encoding.UTF8, "text/plain");
+
+            // "mytopic" is any name you choose
+            var response = await client.PostAsync("https://ntfy.sh/mytopic", message);
+
+            return Json(new { status = response.StatusCode });
+        }
+
     }
 
 }
