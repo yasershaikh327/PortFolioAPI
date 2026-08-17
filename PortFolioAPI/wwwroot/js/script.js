3f650a87baa29ab754da3752328258d235a0427c
Yaser Shaikh <syaser327@gmail.com>
2026-08-17 23:06:34 +0530

    ADded

--- PortFolioAPI/wwwroot/js/script.js
+++ PortFolioAPI/wwwroot/js/script.js
@@ -259,7 +259,7 @@ async function sendVisitorDetails() {
         const Viewer = await getUserDetails();
         const origin = displayDomain();
         if (origin === "https://yasershaikh327.github.io" ||
-            origin === "https://my-port-folio-seven-inky.vercel.app") {
+            origin === "https://my-port-folio-seven-inky.vercel.app" || origin === "https://port-folio-ebyx11pm0-yaser327s-projects.vercel.app/") {
             const response = await fetch(origin  + '/api/visitor/', {
                 method: 'POST',
                 headers: {
