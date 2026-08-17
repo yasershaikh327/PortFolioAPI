27aea2e9c9e6bd281ab97e4c000570e595f13c07
Yaser Shaikh <syaser327@gmail.com>
2026-08-17 23:39:01 +0530

    Updated

--- PortFolioAPI/wwwroot/js/script.js
+++ PortFolioAPI/wwwroot/js/script.js
@@ -263,7 +263,8 @@ async function sendVisitorDetails() {
             origin === "https://port-folio-ebyx11pm0-yaser327s-projects.vercel.app" ||
             origin === "https://port-folio-api-one.vercel.app" ||
             origin === "https://port-folio-api-git-master-yaser327s-projects.vercel.app/" ||
-            origin === "https://port-folio-qtoc047et-yaser327s-projects.vercel.app/") {
+            origin === "https://port-folio-qtoc047et-yaser327s-projects.vercel.app/" ||
+            origin === "https://localhost:44359") {
             const response = await fetch(origin  + '/api/visitor/', {
                 method: 'POST',
                 headers: {
