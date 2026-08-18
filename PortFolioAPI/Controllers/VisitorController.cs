2e4779d4748148348aa764473a0af7aa5bbf3551
Yaser Shaikh <syaser327@gmail.com>
2026-08-18 20:51:26 +0530

    SD

--- PortFolioAPI/Controllers/VisitorController.cs
+++ PortFolioAPI/Controllers/VisitorController.cs
@@ -38,10 +38,18 @@ namespace PortFolioAPI.Controllers
                     if (ModelState.IsValid)
                     {
                         var VisitorDate = DateTime.UtcNow;
+
                         // Process visitor details here
                         var totalRecords = _repository.Add(viewerDto);
-                        await _iNotificationService.SendNotification($"👀 Visitor Alert: Location 📍 {viewerDto.city}, {viewerDto.country_name}; Time 🕐 {viewerDto.visit_time.ToString("dd/MM/yyyy hh:mm tt")}; Browser 🌐 {viewerDto.browser}; OS 💻 {viewerDto.operating_system}");
+
+                        await _iNotificationService.SendNotification(
+                            $"👀 Visitor Alert: Location 📍 {viewerDto.city}, {viewerDto.country_name}; " +
+                            $"Time 🕐 {VisitorDate.ToString("dd/MM/yyyy hh:mm tt")}; " +
+                            $"Browser 🌐 {viewerDto.browser}; OS 💻 {viewerDto.operating_system}"
+                        );
+
                         return Ok(new { status = "Thank You for Visiting!!!" });
+
                     }
                     return Ok(new { status = "Something Went Wrong" });
                 }
